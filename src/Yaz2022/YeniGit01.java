471db95f96d57302875585b9e1470c4fe6602b7f
Ercument0377 <ercument0377@gmail.com>
2022-10-06 15:28:38 +0300

    first commit

---
+++ src/Yaz2022/YeniGit01.java
@@ -0,0 +1,16 @@
+package Yaz2022;
+
+public class YeniGit01 {
+
+	public static void main(String[] args) {
+	
+		int a = 12;
+		int b = 13;
+		System.out.println(a+b);
+		
+
+	}
+
+}
+
+
---
+++ src/gitGitUp/Deneme.java
@@ -0,0 +1,15 @@
+package gitGitUp;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("ilk git komutu");
+
+	}
+
+}
+
+
+
+
+
